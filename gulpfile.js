2425554d7c879fa1a4b26d47cc781f9d7bf2509a
Brad Ringel <brad.ringel@gmail.com>
2016-07-31 15:49:36 -0400

    initial commit after some setup

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var jade = require('gulp-jade');
+var namespace = require('gulp-jade-namespace');
+var sass = require('gulp-sass');
+var bourbon = require('./bower_components/bourbon');
+var neat = require('./bower_components/neat');
+var babel = require('gulp-babel');
+var webserver = require('gulp-webserver');
+
+var sassIncludePaths = [].concat(bourbon.includePaths, neat.includePaths);
+
+gulp.task('polyfills', function () {
+  return gulp.src(['./bower_components/es6-shim/es6-shim.js',
+                   './bower_components/fetch/fetch.js',
+                   './node_modules/jade/**/runtime.js'])
+    .pipe(concat('pollyfills.js'))
+    .pipe(gulp.dest('./dist/scripts'));
+});
+
+gulp.task('jade', function () {
+  return gulp.src('./templates/**/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('./styles/*.scss')
+    .pipe(sass({includePaths: sassIncludePaths}))
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('client-templates', function() {
+  return gulp.src('./client-templates/*.jade')
+    .pipe(jade({ client: true}))
+    .pipe(namespace({namespace: 'jade'}))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('./dist/scripts'));
+});
+
+gulp.task('scripts', function (){
+  return gulp.src('./scripts/*.js')
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(gulp.dest('./dist/scripts'));
+});
+
+gulp.task('server', ['build'], function (){
+  gulp.src('./dist')
+    .pipe(webserver(
+      {liveReload: true, open: true}
+    ));
+});
+
+gulp.task('build', ['polyfills','jade','styles','client-templates']);
